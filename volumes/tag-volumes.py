98729ac812f78444fbd428510d4301867e18e00c
cesarbgoncalves <cgoncalves@bionexo.com>
2020-03-05 12:07:16 -0300

    Alterando script de validação de volumes 2

--- volumes/tag-volumes.py
+++ volumes/tag-volumes.py
@@ -10,25 +10,21 @@ for response in ec2.get_paginator('describe_volumes').paginate():
     volumes.update([(volume['VolumeId'], volume) for volume in response['Volumes']])
     for k, v in volumes.items():
         if 'Tags' in v:
-            # print(f' Volume {v["VolumeId"]}, possui {len(v["Tags"])} TAGs')
-            for tags in v['Tags']:
-                t = 0
-                for validacao in tags_condition:
-                    if validacao == tags_condition[t]:
-                        t += 1
+            # print(f'Volume {v["VolumeId"]}, possui {len(v["Tags"])} TAGs')
+            if v['Tags'] == 'Service' or \
+                    v['Tags'] == 'Environment' or \
+                    v['Tags'] == 'Country' or \
+                    v['Tags'] == 'Product' or \
+                    v['Tags'] == 'Platform' or \
+                    v['Tags'] == 'Owner' or \
+                    v['Tags'] == 'Name':
+                pass
+            else:
+                print(v['VolumeId'])
 
-            # for i in v['Tags']:
+            for tags in v['Tags']:
+                print(tags['Key'])
+            print('=' * 10)
         else:
             print(v["VolumeId"])
-        #
-        #         if i['Key'] == 'Service' or \
-        #                 i['Key'] == 'Environment' or \
-        #                 i['Key'] == 'Country' or \
-        #                 i['Key'] == 'Product' or \
-        #                 i['Key'] == 'Platform' or \
-        #                 i['Key'] == 'Owner' or \
-        #                 i['Key'] == 'Name' or \
-        #                 i['Key'].startswith('aws:'):
-        #             pass
-        #         else:
-        #             print(v["VolumeId"])
+            print('=' * 10)
