689b4977c7e4a677c2bbcca5a56961a75d3bd599
cesarbgoncalves <cgoncalves@bionexo.com>
2020-03-06 15:51:11 -0300

    Alterando script de validação de volumes V1.3

--- volumes/tag-volumes.py
+++ volumes/tag-volumes.py
@@ -1,30 +1,33 @@
 import boto3
+import time
+from time import sleep
+import json
 
 ec2 = boto3.client('ec2')
 
 volumes = {}
 tags_list = []
+vol_tag_fora_padrao = {}
+volume_ok = 0
+entrou_no_else = 0
 # tags_condition = ['Service', 'Environment', 'Country', 'Product', 'Platform', 'Owner']
 
 for response in ec2.get_paginator('describe_volumes').paginate():
     volumes.update([(volume['VolumeId'], volume) for volume in response['Volumes']])
     for k, v in volumes.items():
-        if 'Tags' in v:
-            # print(f'Volume {v["VolumeId"]}, possui {len(v["Tags"])} TAGs')
-            if v['Tags'] == 'Service' or \
-                    v['Tags'] == 'Environment' or \
-                    v['Tags'] == 'Country' or \
-                    v['Tags'] == 'Product' or \
-                    v['Tags'] == 'Platform' or \
-                    v['Tags'] == 'Owner' or \
-                    v['Tags'] == 'Name':
-                pass
-            else:
-                print(v['VolumeId'])
-
-            for tags in v['Tags']:
-                print(tags['Key'])
-            print('=' * 10)
+        if volume_ok == 1:
+            volume_ok = 0
+            continue
+        elif 'Tags' in v:
+            for tag in v['Tags']:
+                if (tag["Key"] == 'Service') or (tag["Key"] == 'Environment') or (tag["Key"] == 'Country') or (
+                        tag["Key"] == 'Product') or (tag["Key"] == 'Platform') or (tag["Key"] == 'Owner') or (
+                        tag["Key"] == 'Name'):
+                    volume_ok += 1
+                    if volume_ok >= 7:
+                        volume_ok = 0
+                        break
         else:
-            print(v["VolumeId"])
-            print('=' * 10)
+            print(k)
+
+
