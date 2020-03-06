1f3b1e8f854407cbcbcc13e5b6226c1f4594a0f3
cesarbgoncalves <cgoncalves@bionexo.com>
2020-03-06 16:18:44 -0300

    Alterando script de validação de volumes V1.3

--- volumes/tag-volumes.py
+++ volumes/tag-volumes.py
@@ -10,7 +10,7 @@ tags_list = []
 vol_tag_fora_padrao = {}
 volume_ok = 0
 entrou_no_else = 0
-# tags_condition = ['Service', 'Environment', 'Country', 'Product', 'Platform', 'Owner']
+tags_condition = ['Service', 'Environment', 'Country', 'Product', 'Platform', 'Owner']
 
 for response in ec2.get_paginator('describe_volumes').paginate():
     volumes.update([(volume['VolumeId'], volume) for volume in response['Volumes']])
@@ -20,14 +20,10 @@ for response in ec2.get_paginator('describe_volumes').paginate():
             continue
         elif 'Tags' in v:
             for tag in v['Tags']:
-                if (tag["Key"] == 'Service') or (tag["Key"] == 'Environment') or (tag["Key"] == 'Country') or (
-                        tag["Key"] == 'Product') or (tag["Key"] == 'Platform') or (tag["Key"] == 'Owner') or (
-                        tag["Key"] == 'Name'):
+                if tag["Key"] in tags_condition:
                     volume_ok += 1
                     if volume_ok >= 7:
                         volume_ok = 0
                         break
         else:
             print(k)
-
-
