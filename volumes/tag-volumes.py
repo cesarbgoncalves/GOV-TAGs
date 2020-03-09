d9b27a5e742d56542c9aec20da71b2aa99b888dd
cesarbgoncalves <cgoncalves@bionexo.com>
2020-03-09 16:06:21 -0300

    Alterando script de validação de volumes V1.5

--- volumes/tag-volumes.py
+++ volumes/tag-volumes.py
@@ -1,28 +1,45 @@
 import boto3
-from time import sleep
 import json
-
 ec2 = boto3.client('ec2')
+tags_condition = ['Environment', 'Country', 'Product', 'Platform', 'Owner']
 
-volumes = {}
-tags_list = []
-vol_tag_fora_padrao = {}
-volume_ok = 0
-entrou_no_else = 0
-tags_condition = ['Service', 'Environment', 'Country', 'Product', 'Platform', 'Owner']
 
-for response in ec2.get_paginator('describe_volumes').paginate():
-    volumes.update([(volume['VolumeId'], volume) for volume in response['Volumes']])
-    for k, v in volumes.items():
-        if volume_ok == 1:
-            volume_ok = 0
-            continue
-        elif 'Tags' in v:
-            for tag in v['Tags']:
-                if tag["Key"] in tags_condition:
-                    volume_ok += 1
-                    if volume_ok >= 7:
-                        volume_ok = 0
-                        break
-        else:
-            print(k)
+def volumes_sem_tags():
+    volumes = {}
+    volumes_sem_tags = []
+    for response in ec2.get_paginator('describe_volumes').paginate():
+        volumes.update([(volume['VolumeId'], volume) for volume in response['Volumes']])
+        for vol_id, value in volumes.items():
+            tags_ok = 0
+            if 'Tags' in value:
+                for tag in value['Tags']:
+                    if tag["Key"] in tags_condition:
+                        tags_ok += 1
+                        if tags_ok == len(tags_condition):
+                            break
+                if tags_ok < len(tags_condition):
+                    volumes_sem_tags.append(vol_id)
+            else:
+                volumes_sem_tags.append(vol_id)
+    return volumes_sem_tags
+
+
+# volumes = {}
+# for response in ec2.get_paginator('describe_volumes').paginate():
+#     volumes.update([(volume['VolumeId'], volume) for volume in response['Volumes']])
+#     print(volume['VolumeId'])
+
+pag = ec2.get_paginator('describe_instances').paginate()
+
+for page in pag:
+    for res in page['Reservations']:
+        for inst in res['Instances']:
+            print(inst['InstanceId'])
+
+# response = ec2.describe_instances()
+# for i in response['Reservations']:
+#     for t in i['Instances']:
+#         if 'Tags' in t:
+#             print(t)
+#         else:
+#             print(f'Não há TAGS na instância {t["InstanceId"]}')
