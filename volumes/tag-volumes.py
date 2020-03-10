b82fe4a368649069ff7de2b5e61fc7a1318522c9
cesarbgoncalves <cgoncalves@bionexo.com>
2020-03-10 17:28:22 -0300

    au

--- volumes/tag-volumes.py
+++ volumes/tag-volumes.py
@@ -1,12 +1,15 @@
 import boto3
 import json
+
 ec2 = boto3.client('ec2')
+ec2resource = boto3.resource('ec2')
 tags_condition = ['Environment', 'Country', 'Product', 'Platform', 'Owner']
 
 
-def volumes_sem_tags():
+def inst_com_volumes_sem_tags():
     volumes = {}
     volumes_sem_tags = []
+    inst_vol_notag = []
     for response in ec2.get_paginator('describe_volumes').paginate():
         volumes.update([(volume['VolumeId'], volume) for volume in response['Volumes']])
         for vol_id, value in volumes.items():
@@ -21,25 +24,102 @@ def volumes_sem_tags():
                     volumes_sem_tags.append(vol_id)
             else:
                 volumes_sem_tags.append(vol_id)
-    return volumes_sem_tags
+            for teste in value['Attachments']:
+                if teste['VolumeId'] in volumes_sem_tags:
+                    inst_vol_notag.append(teste['InstanceId'])
+    # return volumes_sem_tags
+    return inst_vol_notag
 
 
-# volumes = {}
-# for response in ec2.get_paginator('describe_volumes').paginate():
-#     volumes.update([(volume['VolumeId'], volume) for volume in response['Volumes']])
-#     print(volume['VolumeId'])
 
-pag = ec2.get_paginator('describe_instances').paginate()
+def teste():
+    tag_k = 0
+    tag_v = 0
+    tag_instancia = 0
+    response = ec2.describe_instances(InstanceIds=inst_com_volumes_sem_tags())
+    instancias = list(inst_com_volumes_sem_tags())
+    for i in response['Reservations']:
+        for t in i['Instances']:
+            tag_instancia = t['InstanceId']
+            for u in t['Tags']:
+                if u['Key'] in tags_condition:
+                    tag_k = u['Key']
+                    tag_v = u['Value']
+                    # print(u['Key'], u['Value'])
+            print(tag_instancia)
 
-for page in pag:
-    for res in page['Reservations']:
-        for inst in res['Instances']:
-            print(inst['InstanceId'])
+def teste2():
+    dict = {}
+    volumes = ec2resource.volumes.all()
+    for volume in volumes:
+        for vol in volume.attachments:
+            for key, value in dict.items():
 
-# response = ec2.describe_instances()
-# for i in response['Reservations']:
-#     for t in i['Instances']:
-#         if 'Tags' in t:
-#             print(t)
-#         else:
-#             print(f'Não há TAGS na instância {t["InstanceId"]}')
+
+
+teste2()
+
+
+
+
+
+
+# import boto3
+#
+# client = boto3.client('ec2')
+#
+# custom_filter = [{
+#     'Name':'tag:Owner',
+#     'Values': ['user@example.com']}]
+#
+# response = client.describe_instances(Filters=custom_filter)
+
+
+
+
+
+
+
+""" SCRIPT DE TESTE QUE PEGUEI.
+# -----26/07/2019-----#
+# --- Script for update the volume tags in which is not matching with instance tags---
+# !/usr/bin/env python
+
+import boto3
+
+ec2 = boto3.resource('ec2')
+ec2client = boto3.client('ec2')
+
+
+# -----Define Lambda function-----#
+def lambda_handler(event, context):
+    # -----Check& filter Instances which  Kloud_managed equal true-----#
+    instances = ec2client.describe_instances(Filters=[{'Name': 'tag:kloud_managed', 'Values': ['True']}])
+
+    # -----Define dictionary to store Tag Key & value------#
+    dict = {}
+
+    # -----Store Key & Value of Instance Tag:“Name” ------#
+    for reservation in instances['Reservations']:
+        for instance in reservation['Instances']:
+            for tag in instance['Tags']:
+                if tag['Key'] == 'Name':
+                    # print ( instance['InstanceId'],tag['Value'])
+                    # ids.append(( instance['InstanceId'],tag['Value']))
+                    dict[instance['InstanceId']] = tag['Value']
+
+    # -----Store Key & Value with attached instance ID of all volumes ------#     
+    volumes = ec2.volumes.all()
+    for volume in volumes:
+
+        # -----compare dictionary value Key:InstanceID and volume attached Key:InstanceID ------#     
+        for a in volume.attachments:
+            for key, value in dict.items():
+
+            # -----compare dictionary value Key:InstanceID and volume attached Key:InstanceID ------# 
+            # -----If the InstanceID matched create new Tag:’Kloud_Name’ with key of value: servername ------#     
+
+            if a['InstanceId'] == key:
+                volume.create_tags(Tags=[{'Key': 'Kloud_Name', 'Value': value}])
+                
+"""
